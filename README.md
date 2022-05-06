c2d16221a002ba4067c8ab9bd08078dd8f744ec3
Stan Lo <stan001212@gmail.com>
2022-05-06 11:51:06 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Ruby debugger on Replit
+
+This is a demo for trying out Ruby's official debugger [ruby/debug](https://github.com/ruby/debug). 
+
+Once you run 
---
+++ main.rb
@@ -0,0 +1,26 @@
+require "debug"
+
+class Foo
+  def first_call
+    second_call(20)
+  end
+
+  def second_call(num)
+    third_call_with_block do |ten|
+      forth_call(num, ten)
+    end
+  end
+
+  def third_call_with_block(&block)
+    @ivar1 = 10; @ivar2 = 20
+
+    yield(10)
+  end
+
+  def forth_call(num1, num2)
+    debugger
+    num1 + num2
+  end
+end
+
+Foo.new.first_call
